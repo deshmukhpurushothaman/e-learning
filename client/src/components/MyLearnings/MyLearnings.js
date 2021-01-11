060ec41e4d8b8eaa095899dfd40baad09cba2c17
Surenraj <surenraj1201@gmail.com>
2021-01-11 15:27:01 +0530

    ...

--- client/src/components/MyLearnings/MyLearnings.js
+++ client/src/components/MyLearnings/MyLearnings.js
@@ -16,13 +16,13 @@ class MyLearnings extends Component {
                     </div>
                 <div className="container-fluid d-flex justify-content-center">
                  <div className="row">
-                    <div className="col-md-4">
+                    <div className="col">
                         <PythonCard/>
                     </div>
-                    <div className="col-md-4">
+                    <div className="col">
                         <FlutterCard/>
                     </div>
-                    <div className="col-md-4">
+                    <div className="col">
                         <DartCard/>
                     </div>                     
                  </div>   
